56b9ac4d1ca8ab82e09b9dde814a9ec8992203c3
Georg Traar <proddata@users.noreply.github.com>
2026-01-04 16:22:50 +0100

    Move read_ion registration into module

--- src/ion/read_ion_register.cpp
+++ src/ion/read_ion_register.cpp
@@ -1,10 +1,30 @@
 #include "ion/read_ion.hpp"
+#include "duckdb/common/multi_file/multi_file_reader.hpp"
+#include "ion/read_ion_bind.hpp"
+#include "ion/read_ion_scan.hpp"
 
 namespace duckdb {
 namespace ion {
 
 void RegisterReadIon(ExtensionLoader &loader) {
-	(void)loader;
+	TableFunction read_ion("read_ion", {LogicalType::VARCHAR}, ReadIonFunction, ReadIonBind, ReadIonInit,
+	                       ReadIonInitLocal);
+	read_ion.named_parameters["columns"] = LogicalType::ANY;
+	read_ion.named_parameters["format"] = LogicalType::VARCHAR;
+	read_ion.named_parameters["records"] = LogicalType::ANY;
+	read_ion.named_parameters["maximum_depth"] = LogicalType::BIGINT;
+	read_ion.named_parameters["field_appearance_threshold"] = LogicalType::DOUBLE;
+	read_ion.named_parameters["map_inference_threshold"] = LogicalType::BIGINT;
+	read_ion.named_parameters["sample_size"] = LogicalType::BIGINT;
+	read_ion.named_parameters["maximum_sample_files"] = LogicalType::BIGINT;
+	read_ion.named_parameters["union_by_name"] = LogicalType::BOOLEAN;
+	read_ion.named_parameters["conflict_mode"] = LogicalType::VARCHAR;
+	read_ion.named_parameters["profile"] = LogicalType::BOOLEAN;
+	read_ion.named_parameters["use_extractor"] = LogicalType::BOOLEAN;
+	read_ion.projection_pushdown = true;
+	read_ion.filter_pushdown = false;
+	read_ion.filter_prune = false;
+	loader.RegisterFunction(MultiFileReader::CreateFunctionSet(read_ion));
 }
 
 } // namespace ion
--- src/ion_extension.cpp
+++ src/ion_extension.cpp
@@ -3,52 +3,13 @@
 #include "ion_extension.hpp"
 #include "ion_copy.hpp"
 #include "ion_serialize.hpp"
-
-#include "duckdb/common/multi_file/multi_file_reader.hpp"
-
-#include "ion/read_ion_bind.hpp"
-#include "ion/read_ion_scan.hpp"
+#include "ion/read_ion.hpp"
 
 namespace duckdb {
 
-static unique_ptr<FunctionData> IonReadBind(ClientContext &context, TableFunctionBindInput &input,
-                                            vector<LogicalType> &return_types, vector<string> &names) {
-	return ion::ReadIonBind(context, input, return_types, names);
-}
-
-static unique_ptr<GlobalTableFunctionState> IonReadInit(ClientContext &context, TableFunctionInitInput &input) {
-	return ion::ReadIonInit(context, input);
-}
-
-static unique_ptr<LocalTableFunctionState> IonReadInitLocal(ExecutionContext &context, TableFunctionInitInput &input,
-                                                            GlobalTableFunctionState *global_state) {
-	return ion::ReadIonInitLocal(context, input, global_state);
-}
-
-static void IonReadFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
-	ion::ReadIonFunction(context, data_p, output);
-}
-
 static void LoadInternal(ExtensionLoader &loader) {
 	RegisterIonScalarFunctions(loader);
-	TableFunction read_ion("read_ion", {LogicalType::VARCHAR}, IonReadFunction, IonReadBind, IonReadInit,
-	                       IonReadInitLocal);
-	read_ion.named_parameters["columns"] = LogicalType::ANY;
-	read_ion.named_parameters["format"] = LogicalType::VARCHAR;
-	read_ion.named_parameters["records"] = LogicalType::ANY;
-	read_ion.named_parameters["maximum_depth"] = LogicalType::BIGINT;
-	read_ion.named_parameters["field_appearance_threshold"] = LogicalType::DOUBLE;
-	read_ion.named_parameters["map_inference_threshold"] = LogicalType::BIGINT;
-	read_ion.named_parameters["sample_size"] = LogicalType::BIGINT;
-	read_ion.named_parameters["maximum_sample_files"] = LogicalType::BIGINT;
-	read_ion.named_parameters["union_by_name"] = LogicalType::BOOLEAN;
-	read_ion.named_parameters["conflict_mode"] = LogicalType::VARCHAR;
-	read_ion.named_parameters["profile"] = LogicalType::BOOLEAN;
-	read_ion.named_parameters["use_extractor"] = LogicalType::BOOLEAN;
-	read_ion.projection_pushdown = true;
-	read_ion.filter_pushdown = false;
-	read_ion.filter_prune = false;
-	loader.RegisterFunction(MultiFileReader::CreateFunctionSet(read_ion));
+	ion::RegisterReadIon(loader);
 	RegisterIonCopyFunction(loader);
 }
 
