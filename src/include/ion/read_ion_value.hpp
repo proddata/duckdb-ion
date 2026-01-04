149960c8e14055c27a1623865cdf93d87f88cbd9
Georg Traar <proddata@users.noreply.github.com>
2026-01-04 15:27:52 +0100

    Move vectorized value read into read_ion_value

--- src/include/ion/read_ion_value.hpp
+++ src/include/ion/read_ion_value.hpp
@@ -6,8 +6,12 @@
 namespace duckdb {
 namespace ion {
 
+enum class IonVectorReadStatus : uint8_t { UNHANDLED = 0, NULL_VALUE = 1, VALUE_WRITTEN = 2 };
+
 Value IonReadValueImpl(ION_READER *reader, ION_TYPE type);
 void SkipIonValueImpl(ION_READER *reader, ION_TYPE type);
+IonVectorReadStatus ReadIonValueToVectorImpl(ION_READER *reader, ION_TYPE type, Vector &vector, idx_t row,
+                                             const LogicalType &target_type);
 
 } // namespace ion
 } // namespace duckdb
